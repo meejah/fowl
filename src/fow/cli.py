bae30c46d26e57750e1b42b2788675010add960c
meejah <meejah@meejah.ca>
2023-03-20 16:46:40 -0600

    try more packaging things

--- src/fow/cli.py
+++ src/fow/cli.py
@@ -1,6 +1,9 @@
 import click
 
 
+# XXX need to replicate a bunch of "wormhole *" args?
+# e.g. tor stuff, mailbox url, ..
+# 
 @click.group()
 @click.pass_context
 def fow(ctx):
@@ -9,6 +12,24 @@ def fow(ctx):
 
     Bi-directional streaming data over secure and durable Dilated
     magic-wormhole connections.
+    """
+
+
+@fow.command()
+@click.pass_context
+def invite(ctx):
+    """
+    Start a new forwarding session, allocating a code that can be used
+    on another computer to join a forwarding session
+    """
+
+
+@fow.command()
+@click.pass_context
+def accept(ctx):
+    """
+    Join a forwarding session by consuming a wormhole code usually
+    created by 'fow invite'
     """
 
 
