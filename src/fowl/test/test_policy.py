c9656b3fd9b42abccaf4b8c3ccd241ad29574f96
meejah <meejah@meejah.ca>
2024-01-29 12:33:43 -0700

    more test

--- src/fowl/test/test_policy.py
+++ src/fowl/test/test_policy.py
@@ -2,12 +2,12 @@
 
 import pytest
 import ipaddress
-from hypothesis.strategies import ip_addresses, one_of, integers
+from hypothesis.strategies import ip_addresses, one_of, integers, lists
 from hypothesis import given, assume
 
 from twisted.internet.endpoints import TCP4ServerEndpoint, TCP6ServerEndpoint
 
-from fowl.policy import LocalhostAnyPortsPolicy
+from fowl.policy import LocalhostAnyPortsPolicy, LocalhostTcpPortsPolicy
 
 
 def ip_address_to_listener(reactor, port, ipaddr):
@@ -55,3 +55,22 @@ def test_policy_any_bad(reactor, port, ipaddr):
     endpoint = ip_address_to_listener(reactor, port, ipaddr)
     policy = LocalhostAnyPortsPolicy()
     assert policy.can_listen(endpoint) == False, "Should only allow loopback addresses"
+
+
+@given(
+    integers(1, 65536),  # any port we might care about
+    lists(integers(1, 65536)),  # allowed ports in our policy
+    ip_addresses(),
+)
+def test_policy_specific_ports(reactor, port, allowed_ports, ipaddr):
+    """
+    LocalhostAnyPortsPolicy disallows all IP addresses that are NOT
+    localhost
+    """
+    is_local = ipaddress.ip_address(ipaddr).is_loopback
+    is_allowed = port in allowed_ports
+    endpoint = ip_address_to_listener(reactor, port, ipaddr)
+    expected_result = True if is_local and is_allowed else False
+
+    policy = LocalhostTcpPortsPolicy(allowed_ports)
+    assert policy.can_listen(endpoint) == expected_result
