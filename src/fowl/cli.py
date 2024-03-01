d53d2ffb574e4d544dff447dc889b742b7ee84d8
Karel Balej <balejk@matfyz.cz>
2024-03-01 17:06:30 +0100

    fix some typos

--- src/fowl/cli.py
+++ src/fowl/cli.py
@@ -51,7 +51,7 @@ from .messages import (
 @click.pass_context
 def fowld(ctx, ip_privacy, mailbox, debug):
     """
-    Forward Over Wormhole Daemon
+    Forward Over Wormhole, Locally, Daemon
 
     Low-level daemon to set up and forward streams over Dilated magic
     wormhole connections
@@ -168,9 +168,9 @@ def invite(ctx, code_length):
 @click.argument("code")
 def accept(ctx, code):
     """
-    Join an exiting forwarding session.
+    Join an existing forwarding session.
 
-    This consumes an existing invite code (usually created by 'fow
+    This consumes an existing invite code (usually created by 'fowl
     invite')
     """
     ctx.obj = evolve(ctx.obj, code=code)
