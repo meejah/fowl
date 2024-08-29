770d9f8806cbd0578a2666aadfb6ceef33786c79
meejah <meejah@meejah.ca>
2024-08-29 17:00:33 -0600

    a viable strategy

--- src/fowl/test/test_commands.py
+++ src/fowl/test/test_commands.py
@@ -4,12 +4,13 @@ import json
 from io import StringIO
 
 import attrs
-from hypothesis.strategies import ip_addresses, one_of, integers, lists
+from hypothesis.strategies import ip_addresses, one_of, integers, lists, sampled_from, just, builds
 from hypothesis import given, assume, reproduce_failure, settings
 
 from twisted.internet.endpoints import TCP4ServerEndpoint, TCP6ServerEndpoint
 
 from fowl._proto import parse_fowld_command, _Config, fowld_command_to_json
+from fowl.messages import GrantPermission
 
 
 @pytest.fixture()
@@ -48,6 +49,31 @@ def test_roundtrip_grant_permission(listen, connect):
     )
     parsed_cmd = parse_fowld_command(json.dumps(fowld_command_to_json(og_cmd)))
     assert parsed_cmd == og_cmd, "Command mismatch"
+
+
+all_commands = {
+    GrantPermission: {
+        "listen": port_lists(),
+        "connect": port_lists(),
+    },
+}
+
+
+def commands():
+    return one_of([
+        builds(k, **kwargs)
+        for k, kwargs in all_commands.items()
+    ])
+
+
+
+@given(commands())
+def test_roundtrip(og_cmd):
+    print("XXXX", og_cmd)
+    parsed_cmd = parse_fowld_command(json.dumps(fowld_command_to_json(og_cmd)))
+    assert parsed_cmd == og_cmd, "Command mismatch"
+
+
 
 def ___test_command_serialize(config):
     print(config)
