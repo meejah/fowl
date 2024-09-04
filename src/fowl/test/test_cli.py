d91e28f16c304601343704ac8b5ebba47dc74839
meejah <meejah@meejah.ca>
2024-09-03 21:42:19 -0600

    more-working test

--- src/fowl/test/test_cli.py
+++ src/fowl/test/test_cli.py
@@ -20,12 +20,14 @@ from zope.interface import implementer
 from twisted.internet.interfaces import IProcessProtocol
 from twisted.internet.protocol import ProcessProtocol, Protocol, Factory
 from twisted.internet.task import deferLater
-from twisted.internet.defer import ensureDeferred, Deferred, CancelledError
+from twisted.internet.defer import ensureDeferred, Deferred, CancelledError, DeferredList
 from twisted.internet.error import ProcessTerminated
 from fowl._proto import _Config
 from io import StringIO
 import sys
-from fowl.observer import When
+import os
+import signal
+from fowl.observer import When, Framer
 
 
 @implementer(IProcessProtocol)
@@ -38,24 +40,24 @@ class CollectStreams(ProcessProtocol):
             2: b"",
         }
         self._done = When()
+        self._lines = Framer(reactor)
 
     def when_done(self):
         return self._done.when_triggered()
 
-#    def makeConnection(self, process):
-#        pass
+    def next_line(self):
+        return self._lines.next_message()
 
     def childDataReceived(self, fd, data):
         self._streams[fd] += data
-
-    def childConnectionLost(self, fd):
-        pass
+        if fd == 1:
+            self._lines.data_received(data)
 
     def processExited(self, reason):
-        self._done.trigger(self._reactor, reason)
+        pass  #reason == Failure
 
     def processEnded(self, reason):
-        pass  #reason == Failure
+        self._done.trigger(self._reactor, reason)
 
 
 # maybe Hypothesis better, via strategies.binary() ?
@@ -68,41 +70,56 @@ async def test_happy_path(reactor, request, mailbox):
     Improvement: let Hypothesis make up commands, order, etc (how to assert?)
     """
 
-    stdios = [
-        None,
-        None,
-    ]
-
-    def create_stdin0(proto, reactor=None):
-        stdios[0] = FakeStandardIO(proto, reactor, messages=[])
-        return stdios[0]
-
-    def create_stdin1(proto, reactor=None):
-        stdios[1] = FakeStandardIO(proto, reactor, messages=[])
-        return stdios[1]
-
-    config0 = _Config(
-        relay_url=mailbox.url,
-        use_tor=False,
-        create_stdio=create_stdin0,
-        stdout=StringIO(),
-    )
+    print("Starting invite side")
 
     invite_proto = CollectStreams(reactor)
     invite = reactor.spawnProcess(
         invite_proto,
         sys.executable,
         [
-            "python", "-m", "fowl.cli",
-            "invite",
+            "python", "-u", "-m", "fowl.cli",
             "--mailbox", mailbox.url,
-            "--allow-connect", "4321",
-            "--remote", "4444:4321",
-        ]
+            "--allow-connect", "1111",
+            "--remote", "2222:1111",
+        ],
+        env=os.environ,
     )
 
+    line = await invite_proto.next_line()
+    assert line == "Connected."
 
-    try:
-        await invite_proto.when_done()
-    except ProcessTerminated as e:
-        print("failed", invite_proto._streams[2].decode("utf8"))
+    line = await invite_proto.next_line()
+    assert line.startswith("Secret code: "), "Expected secret code"
+    code = line.split(":")[1].strip()
+
+    print(f"Detected code: {code}")
+
+    accept_proto = CollectStreams(reactor)
+    accept = reactor.spawnProcess(
+        accept_proto,
+        sys.executable,
+        [
+            "python", "-u", "-m", "fowl.cli",
+            "--mailbox", mailbox.url,
+            "--allow-listen", "2222",
+            code,
+        ],
+        env=os.environ,
+    )
+
+    print("Starting accept side")
+
+    while True:
+        result, who = await DeferredList(
+            [invite_proto.next_line(), accept_proto.next_line()],
+            fireOnOneCallback=True,
+        )
+        who = "ACC" if who == 1 else "INV"
+        print(f"   {who}: {result.strip()}")
+        if "Listening:" in result:
+            print("  one side is listening")
+            break
+
+    invite.signalProcess(signal.SIGTERM)
+    accept.signalProcess(signal.SIGTERM)
+    print("done")
