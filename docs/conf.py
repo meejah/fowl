9a936045661dcc58b8cb22cb3439dc5da56be5c3
meejah <meejah@meejah.ca>
2025-01-15 23:27:22 -0700

    fix furo config

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'furo'
-html_static_path = ['_static']
+html_logo = 'logo.svg'
+html_static_path = ['.']
 html_theme_options = {
     'logo': 'logo.svg',
     'github_button': 'false',
