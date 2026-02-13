cab7ecaa9f8ab3450d6a6b4d56c324aaa3e5e127
meejah <meejah@meejah.ca>
2026-02-13 00:12:45 -0700

    ruff

--- src/fowl/visual.py
+++ src/fowl/visual.py
@@ -96,11 +96,11 @@ def render_status(st: FowlStatus, time_now, show_logo=True) -> Table:  # Panel? 
     if st.code is not None:
         # only display code until we're connected
         if st.verifier is None:
-            message_text.append(Text(f"code: ") + Text(f"{st.code}", "bold"))
+            message_text.append(Text("code: ") + Text(f"{st.code}", "bold"))
             if int(time_now) % 4 == 0:
-                message_text.append(Text(f"\n      ^-- (waiting for peer)"))
+                message_text.append(Text("\n      ^-- (waiting for peer)"))
             else:
-                message_text.append(Text(f"\n      ^-- tell above code to your peer"))
+                message_text.append(Text("\n      ^-- tell above code to your peer"))
 
     if st.verifier is not None:
         nice_verifier = " ".join(
